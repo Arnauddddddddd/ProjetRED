df4be26fe0a952e9bd27f7bdd41d0202e335e9c5
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-19 15:22:02 +0200

    AgrandissementMapDeFouMalade

--- src/classSelector.go
+++ src/classSelector.go
@@ -22,21 +22,21 @@ func classSelector() {
         
     }
 	if sprite.buttonPlay.Width < rl.GetMouseX() && rl.GetMouseX() < sprite.buttonPlay.Width*2 && screenHeight/4 - sprite.buttonPlay.Height/2 < rl.GetMouseY() && rl.GetMouseY() < screenHeight/4 - sprite.buttonPlay.Height/2 +  sprite.buttonPlay.Height{
-        rl.DrawTexture(sprite.buttonPlayPressed, sprite.buttonPlay.Width, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
-    } else {
         rl.DrawTexture(sprite.buttonPlay, sprite.buttonPlay.Width, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
+    } else {
+        rl.DrawTexture(sprite.buttonPlayPressed, sprite.buttonPlay.Width, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
     }
 
 	if screenWidth/2 - sprite.buttonPlay.Width/2 < rl.GetMouseX() && rl.GetMouseX() < screenWidth/2 + sprite.buttonPlay.Width/2 && screenHeight/4 - sprite.buttonPlay.Height/2 < rl.GetMouseY() && rl.GetMouseY() < screenHeight/4 - sprite.buttonPlay.Height/2 +  sprite.buttonPlay.Height{
-        rl.DrawTexture(sprite.buttonPlayPressed, screenWidth/2 - sprite.buttonPlay.Width/2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
-    } else {
         rl.DrawTexture(sprite.buttonPlay, screenWidth/2 - sprite.buttonPlay.Width/2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
+    } else {
+        rl.DrawTexture(sprite.buttonPlayPressed, screenWidth/2 - sprite.buttonPlay.Width/2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
     }
 
 	if screenWidth - sprite.buttonPlay.Width*2 < rl.GetMouseX() && rl.GetMouseX() < screenWidth - sprite.buttonPlay.Width && screenHeight/4 - sprite.buttonPlay.Height/2 < rl.GetMouseY() && rl.GetMouseY() < screenHeight/4 - sprite.buttonPlay.Height/2 +  sprite.buttonPlay.Height{
-        rl.DrawTexture(sprite.buttonPlayPressed, screenWidth - sprite.buttonPlay.Width*2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
-    } else {
         rl.DrawTexture(sprite.buttonPlay, screenWidth - sprite.buttonPlay.Width*2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
+    } else {
+        rl.DrawTexture(sprite.buttonPlayPressed, screenWidth - sprite.buttonPlay.Width*2, screenHeight/4 - sprite.buttonPlay.Height/2, rl.White)
     }
 
 	rl.ClearBackground(engine.bgColor)
